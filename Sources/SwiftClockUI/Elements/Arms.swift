c5c1b58ce8015cc47a3e0f7785abb294931772a8
Renaud Jenny <renaud.jenny@gmail.com>
2020-02-22 21:19:02 +0000

    feat: add Hover event on Arms to display a drag shadow

--- Sources/SwiftClockUI/Elements/Arms.swift
+++ Sources/SwiftClockUI/Elements/Arms.swift
@@ -2,11 +2,29 @@ import SwiftUI
 import Combine
 
 struct Arms: View {
+    @State var isDragShadowShown = false
+
     var body: some View {
         ZStack {
             ArmView(type: .hour)
             ArmView(type: .minute)
         }
+        .modifier(OnHover(isHover: $isDragShadowShown))
+        .scaleEffect(isDragShadowShown ? 1.1 : 1)
+        .shadow(color: Color(.sRGBLinear, white: 0, opacity: 0.44), radius: isDragShadowShown ? 6 : 0)
+        .animation(.easeInOut)
+    }
+}
+
+struct OnHover: ViewModifier {
+    @Binding var isHover: Bool
+
+    func body(content: Content) -> some View {
+        #if os(macOS)
+        return content.onHover { self.isHover = $0 }
+        #else
+        return content
+        #endif
     }
 }
 
