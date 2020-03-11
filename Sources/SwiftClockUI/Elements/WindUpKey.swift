f15863d5e73fa8de86c0bc990a67ac82842204b2
Renaud Jenny <renaud.jenny@gmail.com>
2020-03-11 19:24:07 +0000

    feat(Elements): add the Steampunk WindUpKey

--- Sources/SwiftClockUI/Elements/WindUpKey.swift
+++ Sources/SwiftClockUI/Elements/WindUpKey.swift
@@ -2,19 +2,47 @@ import SwiftUI
 
 struct WindUpKey: Shape {
     func path(in rect: CGRect) -> Path {
+        let thickness = rect.width * 1/20
+
         var path = Path()
 
-        let holeCenterY = rect.height/3
+        let holeCenterY = rect.height/3 + thickness
         let holeRadius = rect.width/5
         let leftHoleCenter = CGPoint(x: rect.width * 1/4, y: holeCenterY)
+        let rightHoldeCenter = CGPoint(x: rect.width * 3/4, y: holeCenterY)
+        let upperHolerCenter = CGPoint(x: rect.width/2, y:  holeRadius/2 + thickness)
+
+        // Outline
+
+        path.move(to: CGPoint(x: rect.width * 1/3, y: rect.maxY))
+
+        path.addLine(to: CGPoint(x: rect.width * 1/3, y: holeCenterY + holeRadius + thickness))
+
+        path.addArc(center: leftHoleCenter, radius: holeRadius + thickness, startAngle: .radians(.pi/2), endAngle: .radians(.pi * 11/7), clockwise: false)
+
+        path.addArc(center: upperHolerCenter, radius: holeRadius/2 + thickness, startAngle: .radians(.pi), endAngle: .zero, clockwise: false)
+
+        path.addArc(center: rightHoldeCenter, radius: holeRadius + thickness, startAngle: .radians(.pi * 11/8), endAngle: .radians(.pi/2), clockwise: false)
+
+        path.addLine(to: CGPoint(x: rect.width * 2/3, y: holeCenterY + holeRadius + thickness))
+
+        path.addLine(to: CGPoint(x: rect.width * 2/3, y: rect.height))
+
+        path.addLine(to: CGPoint(x: rect.width * 1/3, y: rect.height))
+
+        // Holes
+
+        path.move(to: CGPoint(x: leftHoleCenter.x + holeRadius, y: holeCenterY))
 
         path.addArc(center: leftHoleCenter, radius: holeRadius, startAngle: .zero, endAngle: .radians(2 * .pi), clockwise: false)
 
-        let rightCenter = CGPoint(x: rect.width * 3/4, y: holeCenterY)
+        path.move(to: CGPoint(x: rightHoldeCenter.x + holeRadius, y: holeCenterY))
 
-        path.move(to: CGPoint(x: rightCenter.x + holeRadius, y: holeCenterY))
+        path.addArc(center: rightHoldeCenter, radius: holeRadius, startAngle: .zero, endAngle: .fullRound, clockwise: false)
 
-        path.addArc(center: rightCenter, radius: holeRadius, startAngle: .zero, endAngle: .fullRound, clockwise: false)
+        path.move(to: CGPoint(x: upperHolerCenter.x + holeRadius/2, y: upperHolerCenter.y))
+
+        path.addArc(center: upperHolerCenter, radius: holeRadius/2, startAngle: .zero, endAngle: .fullRound, clockwise: false)
 
         return path
     }
@@ -25,5 +53,6 @@ struct WindUpKey_Previews: PreviewProvider {
         WindUpKey()
             .stroke()
             .aspectRatio(contentMode: .fit)
+            .padding()
     }
 }
