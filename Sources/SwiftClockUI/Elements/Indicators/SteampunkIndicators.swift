9eb1a02675af6742f994b9b0b1808500c105e9b4
Renaud Jenny <renaud.jenny@gmail.com>
2020-03-15 22:31:30 +0000

    feat(Steampunk): add Indicators with Gears and Moon

---
+++ Sources/SwiftClockUI/Elements/Indicators/SteampunkIndicators.swift
@@ -0,0 +1,66 @@
+import SwiftUI
+
+struct SteampunkIndicators: View {
+    var body: some View {
+        ZStack() {
+            Cogwheel()
+                .stroke()
+                .scale(0.8)
+                .modifier(RotateOnAppear())
+            moon
+            gears.mask(moon)
+        }
+        .aspectRatio(1/1, contentMode: .fit)
+    }
+
+    private var moon: some View {
+        ZStack {
+            Moon().stroke()
+            Moon().fill(Color.background)
+        }.modifier(BalanceOnAppear())
+    }
+
+    private var gears: some View {
+        GeometryReader { geometry in
+            Cogwheel(toothCount: 12, armCount: 3, addExtraHoles: false)
+                .scale(1/5)
+                .stroke()
+                .modifier(RotateOnAppear(clockwise: false))
+                .position(x: geometry.diameter * 1/10, y: geometry.diameter * 10/15)
+            Cogwheel(toothCount: 8, armCount: 5, addExtraHoles: false)
+                .scale(1/6)
+                .fill(style: .init(eoFill: true, antialiased: true))
+                .modifier(RotateOnAppear(clockwise: true))
+                .position(x: geometry.diameter * 7/29, y: geometry.diameter * 7/9)
+            Cogwheel(toothCount: 12, armCount: 8, addExtraHoles: false)
+                .scale(1/4)
+                .stroke()
+                .modifier(RotateOnAppear(clockwise: false))
+                .position(x: geometry.diameter * 2/5, y: geometry.diameter * 9/10)
+        }
+    }
+}
+
+struct BalanceOnAppear: ViewModifier {
+    @Environment(\.clockIsAnimationEnabled) var isAnimationEnabled
+    @State var animate = false
+    var clockwise = true
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(rotationAngle)
+            .animation(Animation.linear(duration: 4).repeatForever(autoreverses: true))
+            .onAppear { self.animate = true }
+    }
+
+    var rotationAngle: Angle {
+        guard isAnimationEnabled else { return .zero }
+        return animate ? .degrees(20) : -.degrees(20)
+    }
+}
+
+struct SteampunkIndicators_Previews: PreviewProvider {
+    static var previews: some View {
+        SteampunkIndicators().padding()
+    }
+}
--- Sources/SwiftClockUI/ViewModifiers/RotateOnAppear.swift
+++ Sources/SwiftClockUI/ViewModifiers/RotateOnAppear.swift
@@ -8,7 +8,7 @@ struct RotateOnAppear: ViewModifier {
     func body(content: Content) -> some View {
         content
             .rotationEffect(rotationAngle)
-            .animation(Animation.easeInOut(duration: 4).repeatForever(autoreverses: true))
+            .animation(Animation.linear(duration: 4).repeatForever(autoreverses: false))
             .onAppear { self.animate = true }
     }
 
