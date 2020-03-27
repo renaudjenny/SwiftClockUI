efbe585ad4916dfe773830153b2277b0662a6791
Renaud Jenny <renaud.jenny@gmail.com>
2020-03-27 17:29:32 +0000

    refactor(Steampunk): improve code readability of WindUpKey

--- Sources/SwiftClockUI/Elements/Steampunk/WindUpKey.swift
+++ Sources/SwiftClockUI/Elements/Steampunk/WindUpKey.swift
@@ -6,34 +6,45 @@ struct WindUpKey: Shape {
         let thickness = width * 1/12
         let holeRadius = width/6
         let holeCenterY = holeRadius/2 + width/4 + thickness
-        let leftHoleCenter = CGPoint(x: width * 1/4, y: holeCenterY)
-        let rightHoleCenter = CGPoint(x: width * 3/4, y: holeCenterY)
-        let upperHoleCenter = CGPoint(x: width/2, y:  holeRadius/2 + thickness)
+        let leftHole = CGRect.circle(center: CGPoint(x: width * 1/4, y: holeCenterY), radius: holeRadius)
+        let rightHole = CGRect.circle(center: CGPoint(x: width * 3/4, y: holeCenterY), radius: holeRadius)
+        let topHole = CGRect.circle(center: CGPoint(x: width/2, y: holeRadius/2 + thickness), radius: holeRadius/2)
 
         var path = Path()
-        outline(path: &path, width: width, maxY: rect.maxY, thickness: thickness, holeRadius: holeRadius, holeCenterY: holeCenterY, leftHoleCenter: leftHoleCenter, rightHoleCenter: rightHoleCenter, upperHoleCenter: upperHoleCenter)
-        holes(path: &path, holeRadius: holeRadius, holeCenterY: holeCenterY, leftHoleCenter: leftHoleCenter, rightHoleCenter: rightHoleCenter, upperHoleCenter: upperHoleCenter)
+        addOutline(to: &path, rect: rect, thickness: thickness, leftHole: leftHole, rightHole: rightHole, topHole: topHole)
+        path.addCircle(leftHole)
+        path.addCircle(rightHole)
+        path.addCircle(topHole)
         return path
     }
 
-    private func outline(path: inout Path, width: CGFloat, maxY: CGFloat, thickness: CGFloat, holeRadius: CGFloat, holeCenterY: CGFloat, leftHoleCenter: CGPoint, rightHoleCenter: CGPoint, upperHoleCenter: CGPoint) {
-        path.move(to: CGPoint(x: width * 1/3, y: maxY))
-        path.addLine(to: CGPoint(x: width * 1/3, y: holeCenterY + holeRadius + thickness))
-        path.addArc(center: leftHoleCenter, radius: holeRadius + thickness, startAngle: .radians(.pi/2), endAngle: .radians(.pi * 3/2), clockwise: false)
-        path.addArc(center: upperHoleCenter, radius: holeRadius/2 + thickness, startAngle: .radians(.pi), endAngle: .zero, clockwise: false)
-        path.addArc(center: rightHoleCenter, radius: holeRadius + thickness, startAngle: .radians(.pi * 3/2), endAngle: .radians(.pi/2), clockwise: false)
-        path.addLine(to: CGPoint(x: width * 2/3, y: holeCenterY + holeRadius + thickness))
-        path.addLine(to: CGPoint(x: width * 2/3, y: maxY))
+    private func addOutline(to path: inout Path, rect: CGRect, thickness: CGFloat, leftHole: CGRect, rightHole: CGRect, topHole: CGRect) {
+        path.move(to: CGPoint(x: rect.width * 1/3, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.width * 1/3, y: leftHole.maxY + thickness))
+        path.addArc(center: leftHole.center, radius: leftHole.radius + thickness, startAngle: .radians(.pi/2), endAngle: .radians(.pi * 3/2), clockwise: false)
+        path.addArc(center: topHole.center, radius: topHole.radius + thickness, startAngle: .radians(.pi), endAngle: .zero, clockwise: false)
+        path.addArc(center: rightHole.center, radius: rightHole.radius + thickness, startAngle: .radians(.pi * 3/2), endAngle: .radians(.pi/2), clockwise: false)
+        path.addLine(to: CGPoint(x: rect.width * 2/3, y: rightHole.maxY + thickness))
+        path.addLine(to: CGPoint(x: rect.width * 2/3, y: rect.maxY))
         path.closeSubpath()
     }
+}
 
-    private func holes(path: inout Path, holeRadius: CGFloat, holeCenterY: CGFloat, leftHoleCenter: CGPoint, rightHoleCenter: CGPoint, upperHoleCenter: CGPoint) {
-        path.move(to: CGPoint(x: leftHoleCenter.x + holeRadius, y: holeCenterY))
-        path.addArc(center: leftHoleCenter, radius: holeRadius, startAngle: .zero, endAngle: .radians(2 * .pi), clockwise: false)
-        path.move(to: CGPoint(x: rightHoleCenter.x + holeRadius, y: holeCenterY))
-        path.addArc(center: rightHoleCenter, radius: holeRadius, startAngle: .zero, endAngle: .fullRound, clockwise: false)
-        path.move(to: CGPoint(x: upperHoleCenter.x + holeRadius/2, y: upperHoleCenter.y))
-        path.addArc(center: upperHoleCenter, radius: holeRadius/2, startAngle: .zero, endAngle: .fullRound, clockwise: false)
+// TODO: move this extension into its own file
+extension CGRect {
+    static func circle(center: CGPoint, radius: CGFloat) -> Self {
+        .init(x: center.x - radius, y: center.y - radius, width: radius * 2, height: radius * 2)
+    }
+
+    var center: CGPoint { .init(x: midX, y: midY) }
+    var radius: CGFloat { min(width, height)/2 }
+}
+
+// TODO: move this extension into its own file
+extension Path {
+    mutating func addCircle(_ circle: CGRect) {
+        move(to: CGPoint(x: circle.maxX, y: circle.center.y))
+        addArc(center: circle.center, radius: circle.radius, startAngle: .zero, endAngle: .fullRound, clockwise: false)
     }
 }
 
