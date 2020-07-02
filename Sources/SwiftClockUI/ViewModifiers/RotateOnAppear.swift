12a4246c49dba46903b16319bfb04103fa82069e
Renaud Jenny <renaud.jenny@gmail.com>
2020-07-02 08:21:11 +0100

    fix(Steampunk): use WithAnimation for RotateOnAppear
    * This fix the problem where multiplie animations occurred while
    manipulating RotateOnAppear

--- Sources/SwiftClockUI/ViewModifiers/RotateOnAppear.swift
+++ Sources/SwiftClockUI/ViewModifiers/RotateOnAppear.swift
@@ -2,17 +2,26 @@ import SwiftUI
 
 struct RotateOnAppear: ViewModifier {
     @Environment(\.clockIsAnimationEnabled) var isAnimationEnabled
-    @State var animate = false
+    @State private var animate = false
     var clockwise = true
 
     func body(content: Content) -> some View {
         content
             .rotationEffect(rotationAngle)
-            .animation(Animation.linear(duration: 4).repeatForever(autoreverses: false))
-            .onAppear { self.animate = true }
+            .onAppear {
+                withAnimation(self.animation) {
+                    self.animate = true
+                }
+            }
     }
 
-    var rotationAngle: Angle {
+    private var animation: Animation {
+        Animation
+            .linear(duration: 4)
+            .repeatForever(autoreverses: false)
+    }
+
+    private var rotationAngle: Angle {
         guard isAnimationEnabled else { return .zero }
         if clockwise {
             return animate ? .fullRound : .zero
