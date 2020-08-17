35a740876baf7c72228305cd70a5ecca167148f6
Renaud Jenny <renaud.jenny@gmail.com>
2020-08-17 09:30:13 +0100

    refactor(Borders): use convenient modifier instead of GeometryReader
    ...for Art Nouveau Clock Border

--- Sources/SwiftClockUI/Elements/Borders/ArtNouveauClockBorder.swift
+++ Sources/SwiftClockUI/Elements/Borders/ArtNouveauClockBorder.swift
@@ -3,20 +3,21 @@ import SwiftUI
 struct ArtNouveauClockBorder: View {
     static let borderWidthRatio: CGFloat = 1/50
     static let innerCircleScale: CGFloat = 9/10
+    @State private var circle: CGRect = .zero
 
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                Circle()
-                    .stroke(lineWidth: geometry.radius * Self.borderWidthRatio)
-                Circle()
-                    .scale(Self.innerCircleScale)
-                    .transform(.init(
-                        translationX: 0,
-                        y: geometry.radius * (1 - Self.innerCircleScale)))
-                    .stroke(lineWidth: geometry.radius * Self.borderWidthRatio/2)
-            }
+        ZStack {
+            Circle()
+                .stroke(lineWidth: circle.radius * Self.borderWidthRatio)
+            Circle()
+                .scale(Self.innerCircleScale)
+                .transform(.init(
+                    translationX: 0,
+                    y: circle.radius * (1 - Self.innerCircleScale)
+                ))
+                .stroke(lineWidth: circle.radius * Self.borderWidthRatio/2)
         }
+        .modifier(LocalFrameProvider(frame: $circle))
     }
 }
 
