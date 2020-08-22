0d9a23bc8507624bb162cce496c051ae554d4463
Renaud Jenny <renaud.jenny@gmail.com>
2020-08-22 08:32:55 +0100

    refactor(Tests): use SwiftUI Snapshot directly in Arm tests
    * Instead of using an explicit intermediate UIHostingController

--- Tests/SwiftClockUITests/Elements/Arm/ArmTests.swift
+++ Tests/SwiftClockUITests/Elements/Arm/ArmTests.swift
@@ -11,35 +11,30 @@ class ArmTests: XCTestCase {
 
   func testHourArm() {
     let arms = ArmHour_Previews.previews.environment(\.clockIsAnimationEnabled, false)
-    let hostingController = UIHostingController(rootView: arms)
-    assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe))
+    assertSnapshot(matching: arms, as: .default)
   }
 
   func testArmWith25MinuteAngle() {
     let arms = ArmWith25MinuteAngle_Previews.previews.environment(\.clockIsAnimationEnabled, false)
-    let hostingController = UIHostingController(rootView: arms)
-    assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe))
+    assertSnapshot(matching: arms, as: .default)
   }
 
   func testArtNouveauArm() {
     let arms = ArtNouveauDesignArm_Previews.previews.environment(\.clockIsAnimationEnabled, false)
-    let hostingController = UIHostingController(rootView: arms)
-    assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe))
+    assertSnapshot(matching: arms, as: .default)
   }
 
   func testDrawningArm() {
     let arms = DrawingDesignArm_Previews.previews
         .environment(\.clockIsAnimationEnabled, false)
         .environment(\.clockRandom, .fixed)
-    let hostingController = UIHostingController(rootView: arms)
-    assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe))
+    assertSnapshot(matching: arms, as: .default)
   }
 
   func testDrawnArms() {
     let arms = DrawnArm_Previews.previews
         .environment(\.clockIsAnimationEnabled, false)
         .environment(\.clockRandom, .fixed)
-    let hostingController = UIHostingController(rootView: arms)
-    assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe))
+    assertSnapshot(matching: arms, as: .default)
   }
 }
