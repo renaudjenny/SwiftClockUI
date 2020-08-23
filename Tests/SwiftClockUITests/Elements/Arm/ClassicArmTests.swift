85cd49c3ccb26ae89001b41e74e5afa67c302140
Renaud Jenny <renaud.jenny@gmail.com>
2020-08-23 08:10:25 +0100

    refactor(Tests): change missing UIHostingController in Arms tests
    * Instead of using an explicit intermediate UIHostingController

--- Tests/SwiftClockUITests/Elements/Arm/ClassicArmTests.swift
+++ Tests/SwiftClockUITests/Elements/Arm/ClassicArmTests.swift
@@ -6,7 +6,6 @@ import SwiftUI
 class ClassicArmTests: XCTestCase {
     func testClassicArm() {
         let arms = ClassicArm_Previews.previews.environment(\.clockIsAnimationEnabled, false)
-        let hostingController = UIHostingController(rootView: arms)
-        assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe))
+        assertSnapshot(matching: arms, as: .default)
     }
 }
