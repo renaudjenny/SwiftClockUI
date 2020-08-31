85460d3a071e42d0d6263e04e7cae5b20da2ad54
Renaud Jenny <renaud.jenny@gmail.com>
2020-08-31 21:05:17 +0100

    fix(Indicators): get rid of LocalFrameProvider in ClassicIndicators

--- Sources/SwiftClockUI/Elements/Borders/ClassicClockBorder.swift
+++ Sources/SwiftClockUI/Elements/Borders/ClassicClockBorder.swift
@@ -4,11 +4,13 @@ struct ClassicClockBorder: View {
     static let borderWidthRatio: CGFloat = 1/35
 
     var body: some View {
-        GeometryReader {
-            Circle()
-                .stroke(lineWidth: $0.radius * Self.borderWidthRatio)
-                .modifier(ScaleUpOnAppear())
-        }
+        GeometryReader(content: content)
+    }
+
+    private func content(geometry: GeometryProxy) -> some View {
+        Circle()
+            .stroke(lineWidth: geometry.radius * Self.borderWidthRatio)
+            .modifier(ScaleUpOnAppear())
     }
 }
 
--- Sources/SwiftClockUI/Elements/Indicators/ClassicIndicators.swift
+++ Sources/SwiftClockUI/Elements/Indicators/ClassicIndicators.swift
@@ -50,38 +50,40 @@ private struct HourTexts: View {
 private struct HourIndicators: View {
     private static let hourDotRatio: CGFloat = 2/35
     let marginRatio: CGFloat
-    @State private var circle: CGRect = .zero
 
     var body: some View {
-        ForEach(1..<13, content: hour).modifier(LocalFrameProvider(frame: $circle))
+        GeometryReader(content: content)
     }
 
-    private func hour(_ hour: Int) -> some View {
-        Circle()
-            .frame(width: circle.radius * Self.hourDotRatio)
-            .modifier(PositionInCircle(
-                angle: .degrees(Double(hour) * .hourInDegree),
-                marginRatio: marginRatio/3
-            ))
+    private func content(geometry: GeometryProxy) -> some View {
+        ForEach(1..<13) { hour in
+            Circle()
+                .frame(width: geometry.radius * Self.hourDotRatio)
+                .modifier(PositionInCircle(
+                    angle: .degrees(Double(hour) * .hourInDegree),
+                    marginRatio: self.marginRatio/3
+                ))
+        }
     }
 }
 
 private struct MinuteIndicators: View {
     private static let minuteDotRatio: CGFloat = 1/35
     let marginRatio: CGFloat
-    @State private var circle: CGRect = .zero
 
     var body: some View {
-        ForEach(1..<61, content: minute).modifier(LocalFrameProvider(frame: $circle))
+        GeometryReader(content: content)
     }
 
-    private func minute(_ minute: Int) -> some View {
-        Circle()
-            .frame(width: circle.radius * Self.minuteDotRatio)
-            .modifier(PositionInCircle(
-                angle: .degrees(Double(minute) * .minuteInDegree),
-                marginRatio: marginRatio/3
-            ))
+    private func content(geometry: GeometryProxy) -> some View {
+        ForEach(1..<61) { minute in
+            Circle()
+                .frame(width: geometry.radius * Self.minuteDotRatio)
+                .modifier(PositionInCircle(
+                    angle: .degrees(Double(minute) * .minuteInDegree),
+                    marginRatio: self.marginRatio/3
+                ))
+        }
     }
 }
 
