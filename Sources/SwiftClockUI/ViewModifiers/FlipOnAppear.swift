d59932f909c53a635325e64be32589af491796c3
Renaud Jenny <renaud.jenny@gmail.com>
2020-09-02 19:06:17 +0100

    refactor(ViewModifiers): use explicit animation for FlipOnAppear

--- Sources/SwiftClockUI/ViewModifiers/FlipOnAppear.swift
+++ Sources/SwiftClockUI/ViewModifiers/FlipOnAppear.swift
@@ -2,12 +2,26 @@ import SwiftUI
 
 struct FlipOnAppear: ViewModifier {
     @Environment(\.clockIsAnimationEnabled) var isAnimationEnabled
-    @State var animate = false
+    @State var rotationAngle: Angle = .zero
 
     func body(content: Content) -> some View {
         content
-            .rotation3DEffect(animate && isAnimationEnabled ? .fullRound : .zero, axis: (x: 1, y: 1, z: 0))
-            .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true), value: animate)
-            .onAppear { self.animate = true }
+            .rotation3DEffect(rotationAngle, axis: (x: 1, y: 1, z: 0))
+            .onAppear {
+                guard self.isAnimationEnabled else { return }
+
+                let animation = Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)
+                withAnimation(animation) {
+                    self.rotationAngle = .fullRound
+                }
+        }
+    }
+}
+
+struct FlipOnAppear_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("Hello, World!")
+            .rotationEffect(.degrees(-45))
+            .modifier(FlipOnAppear())
     }
 }
