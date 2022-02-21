70af197db790d2c2a38e00f017f764d2f971b5e6
Renaud Jenny <renaud.jenny@gmail.com>
2022-02-21 20:39:42 +0000

    fix(Steampunk): attempt to fix Cogwheel tests
    * Add 99% precision for Cogwheel snapshot test. The imprecision is
      probably due to the Simulator snapshot machine that isn't exactly the
      same (i386 Simulator vs ARM Simulator?)

--- Tests/SwiftClockUITests/Elements/Steampunk/CogwheelTests.swift
+++ Tests/SwiftClockUITests/Elements/Steampunk/CogwheelTests.swift
@@ -8,14 +8,14 @@ class CogwheelKeyTests: XCTestCase {
     func testCogwheel() {
         assertSnapshot(
             matching: UIHostingController(rootView: Cogwheel_Previews.previews),
-            as: .image(on: .iPhoneSe)
+            as: .image(on: .iPhoneSe, precision: 99/100)
         )
     }
 
     func testCogwheels() {
         assertSnapshot(
             matching: UIHostingController(rootView: Cogwheels_Previews.previews),
-            as: .image(on: .iPhoneSe)
+            as: .image(on: .iPhoneSe, precision: 99/100)
         )
     }
     #endif
