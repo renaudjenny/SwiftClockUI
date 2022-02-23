cea613541372f73cdaaef199daae4252be387da3
Renaud Jenny <renaud.jenny@gmail.com>
2022-02-23 23:33:48 +0000

    update(Steampunk): improve Indicators code by removing `GeometryReader`
    * Avoiding relying on `GeometryReader` in Indicators, avoiding some
      issues and simplifying the code

--- Sources/SwiftClockUI/Elements/Indicators/SteampunkIndicators.swift
+++ Sources/SwiftClockUI/Elements/Indicators/SteampunkIndicators.swift
@@ -2,59 +2,36 @@ import SwiftUI
 
 struct SteampunkIndicators: View {
     @Environment(\.clockConfiguration) var configuration
-    static let marginRatio: CGFloat = 1/5
-    static let decorationScale: CGFloat = 7/10
-    static let lineWidthRatio: CGFloat = 1/100
-    static let decorationLineWidthRatio: CGFloat = lineWidthRatio * 1/decorationScale
 
-    @State private var clowiseRotationAngle: Angle = .zero
-    @State private var counterClockwiseRotationAngle: Angle = .zero
+    @State private var animationRotationAngle: Angle = .zero
 
     var body: some View {
-        GeometryReader(content: content)
-            .onAppear {
-                DispatchQueue.main.async {
-                    withAnimation(.linear(duration: 4).repeatForever(autoreverses: false)) {
-                        clowiseRotationAngle += .fullRound
-                        counterClockwiseRotationAngle += -.fullRound
-                    }
-                }
-            }
-    }
-
-    private func content(geometry: GeometryProxy) -> some View {
         ZStack {
             ZStack {
-                mainCogwheel(geometry: geometry)
-                moon(geometry: geometry)
-                gears().mask(moon(geometry: geometry))
-            }.scaleEffect(Self.decorationScale)
-            circles(geometry: geometry)
+                Cogwheel(angle: animationRotationAngle).scale(56/100).stroke()
+                ZStack {
+                    Moon().scale(70/100).fill(Color.background)
+                    Moon().scale(70/100).stroke()
+                }
+                gears().mask(Moon().scale(70/100))
+            }
+            circles()
             numbers
         }
-    }
-
-    private func mainCogwheel(geometry: GeometryProxy) -> some View {
-        Cogwheel(angle: clowiseRotationAngle)
-            .scale(0.8)
-            .stroke(lineWidth: geometry.radius * Self.decorationLineWidthRatio)
-    }
-
-    private func circles(geometry: GeometryProxy) -> some View {
-        ZStack {
-            Circle()
-                .scale(21/25)
-                .stroke(lineWidth: geometry.radius * Self.lineWidthRatio)
-            Circle()
-                .scale(20/25)
-                .stroke(lineWidth: geometry.radius * Self.lineWidthRatio)
+        .onAppear {
+            guard animationRotationAngle == .zero else { return }
+            DispatchQueue.main.async {
+                withAnimation(.linear(duration: 4).repeatForever(autoreverses: false)) {
+                    animationRotationAngle += .fullRound
+                }
+            }
         }
     }
 
-    private func moon(geometry: GeometryProxy) -> some View {
+    private func circles() -> some View {
         ZStack {
-            Moon().fill(Color.background)
-            Moon().stroke(lineWidth: geometry.radius * Self.decorationLineWidthRatio)
+            Circle().scale(21/25).stroke()
+            Circle().scale(20/25).stroke()
         }
     }
 
@@ -62,7 +39,9 @@ struct SteampunkIndicators: View {
         ForEach(RomanNumber.numbers(configuration: configuration), id: \.self) { romanNumber in
             Plate(type: self.plateType(for: romanNumber), text: romanNumber)
                 .scaleEffect(3/19)
-                .modifier(PositionInCircle(angle: RomanNumber.angle(for: romanNumber), marginRatio: Self.marginRatio))
+                .modifier(
+                    PositionInCircle(angle: RomanNumber.angle(for: romanNumber), marginRatio: 1/5)
+                )
         }
     }
 
@@ -90,8 +69,8 @@ struct SteampunkIndicators: View {
                     scale: 1.0/4
                 )
             ],
-            angle: clowiseRotationAngle
-        ).stroke()
+            angle: animationRotationAngle
+        ).scale(70/100).stroke()
     }
 }
 
