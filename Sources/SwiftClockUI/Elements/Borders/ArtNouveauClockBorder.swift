b2a214033be1939b72cbab2fd97db479aa4aa098
Renaud Jenny <renaud.jenny@gmail.com>
2022-03-09 22:15:52 +0000

    refactor(ArtNouveau): limit usage of `GeometryReader` in Border
    * Use Shape instead

--- Sources/SwiftClockUI/Elements/Borders/ArtNouveauClockBorder.swift
+++ Sources/SwiftClockUI/Elements/Borders/ArtNouveauClockBorder.swift
@@ -1,32 +1,39 @@
 import SwiftUI
 
 struct ArtNouveauClockBorder: View {
-    static let borderWidthRatio: CGFloat = 1/50
-    static let innerCircleScale: CGFloat = 9/10
-
     var body: some View {
-        GeometryReader(content: content)
+        ZStack {
+            Circle().strokeBorder(lineWidth: 2)
+            InnerCircle().strokeBorder()
+        }
+    }
+}
+
+private struct InnerCircle: InsettableShape {
+    var insetAmount = 0.0
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addCircle(CGRect(
+            x: rect.midX - rect.radius * 90/100,
+            y: rect.midY + rect.radius * 10/100 - rect.radius * 90/100,
+            width: rect.radius * 2 * 90/100,
+            height: rect.radius * 2 * 90/100
+        ).insetBy(dx: insetAmount, dy: insetAmount))
+        return path
     }
 
-    private func content(geometry: GeometryProxy) -> some View {
-        ZStack {
-            Circle()
-                .strokeBorder(lineWidth: geometry.radius * Self.borderWidthRatio)
-            Circle()
-                .strokeBorder(lineWidth: geometry.radius * Self.borderWidthRatio/2)
-                .scaleEffect(Self.innerCircleScale)
-                .transformEffect(.init(
-                    translationX: 0,
-                    y: geometry.radius * (1 - Self.innerCircleScale)
-                ))
-        }
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var circle = self
+        circle.insetAmount += amount
+        return circle
     }
 }
 
 #if DEBUG
 struct ArtNouveauClockBorderClockBorder_Previews: PreviewProvider {
     static var previews: some View {
-        ArtNouveauClockBorder().padding()
+        ArtNouveauClockBorder()
     }
 }
 #endif
