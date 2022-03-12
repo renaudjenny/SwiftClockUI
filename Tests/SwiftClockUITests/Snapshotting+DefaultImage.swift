6ca111c6758b4bafd18bc90a981686a28915772f
Renaud Jenny <renaud.jenny@gmail.com>
2022-03-12 19:27:40 +0000

    update(CI): remove snapshot precision for the default image mode

--- Tests/SwiftClockUITests/Snapshotting+DefaultImage.swift
+++ Tests/SwiftClockUITests/Snapshotting+DefaultImage.swift
@@ -3,8 +3,6 @@ import SnapshotTesting
 
 #if !os(macOS)
 extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
-    static var `default`: Self {
-        image(precision: 95/100, layout: .device(config: .iPhoneSe))
-    }
+    static var `default`: Self { image(layout: .device(config: .iPhoneSe)) }
 }
 #endif
