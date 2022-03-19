c7ebca412ef575f9a653cf38ff88bbef702a2b37
Renaud Jenny <renaud.jenny@gmail.com>
2022-03-19 19:11:42 +0000

    test(CI): attempt to find the best precision value possible
    * Screenshots can slightly vary depending on the platform, iOS version
      and simulator supported (putting iPhone SE 1st Gen is not sufficient
      to content most of the case unfortunately)

--- Tests/SwiftClockUITests/Snapshotting+DefaultImage.swift
+++ Tests/SwiftClockUITests/Snapshotting+DefaultImage.swift
@@ -3,6 +3,6 @@ import SnapshotTesting
 
 #if !os(macOS)
 extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
-    static var `default`: Self { image(layout: .device(config: .iPhoneSe)) }
+    static var `default`: Self { image(precision: 99.9/100, layout: .device(config: .iPhoneSe)) }
 }
 #endif
